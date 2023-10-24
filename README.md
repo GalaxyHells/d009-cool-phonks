a3fdf4e68bb09018bf4ec58a18c8ea3fcf4e3396
GalaxyHells <fabricio_gohan@hotmail.com>
2023-10-24 20:06:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# d009-cool-phonks
+ Desafio 09 do Curso em Video
---
+++ js/randomize.js
@@ -0,0 +1,21 @@
+
+// Elementos
+const randomizeButton = document.querySelector("button#randomize")
+
+function randomizePhonk () {
+    let min = 1
+    let max = 4
+    let randomPage = Math.floor(Math.random() * (max - min + 1) + min)
+
+    window.location = "../html/sub-pag0" + randomPage + ".html"
+}
+
+function loadVideoData () {
+
+}
+
+randomizeButton.addEventListener("click", e => {
+    e.preventDefault();
+
+    randomizePhonk();
+})
